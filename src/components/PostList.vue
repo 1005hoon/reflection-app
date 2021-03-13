b03dfa5e5e13a79b4c6398142363a5a64e122d4e
Hoon <shoh@help-me.kr>
2021-03-14 01:21:33 +0900

    ADD: use props in setup function for post

---
+++ src/components/PostList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="post-lists">
+    <h1>this is post lists</h1>
+    <div v-for="post in posts" :key="post">
+      <Post :post="post" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Post from './Post'
+
+export default {
+  props: ['posts'],
+  components: { Post },
+  setup(props) {
+    console.log(props.posts)
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from 'vue'
+import PostList from '../components/PostList'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: { PostList },
+  setup() {
+    const posts = ref([
+      {
+        id: 1,
+        title: '땅콩이 탈모 치료하기',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+      }, 
+      {
+        id: 2,
+        title: '이력서 작성하기',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.'
+      }
+    ])
+    return { posts }
+  },
 }
 </script>
